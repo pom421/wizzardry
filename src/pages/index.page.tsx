9ed3fb9ab302c3b1c841f62bd34cf3b02d0fdc76
pom421 <mauguet.po@gmail.com>
2022-09-16 14:25:44 +0200

    feat: placholder for the call of the machine

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,7 +1,29 @@
+import { useMachine } from "@xstate/react"
 import type { NextPage } from "next"
+import { declarationMachine } from "../machines/declarationMachine"
 
 const Home: NextPage = () => {
-  return <div>Hello world</div>
+  const [state, send] = useMachine(declarationMachine)
+
+  return (
+    <div>
+      {JSON.stringify(state.value)}
+      <button
+        onClick={() => {
+          send("MOUSEOVER")
+        }}
+      >
+        Mouse hover
+      </button>
+      <button
+        onClick={() => {
+          send("MOUSEOUT")
+        }}
+      >
+        Mouse out
+      </button>
+    </div>
+  )
 }
 
 export default Home
