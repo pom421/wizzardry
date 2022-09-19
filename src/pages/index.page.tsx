924e98884c53eb5c5aa3c204608da45bb9298a4e
pom421 <mauguet.po@gmail.com>
2022-09-19 18:08:44 +0200

    refactor: remove useless code

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,31 +1,7 @@
-import { useMachine } from "@xstate/react"
 import type { NextPage } from "next"
-import { declarationMachine } from "../machines/firstMachine"
 
 const Home: NextPage = () => {
-  const [state, send] = useMachine(declarationMachine)
-
-  return (
-    <div>
-      {JSON.stringify(state.value)} <br />
-      {JSON.stringify(state.context)}
-      <br />
-      <button
-        onClick={() => {
-          send("previous")
-        }}
-      >
-        Previous
-      </button>
-      <button
-        onClick={() => {
-          send("next")
-        }}
-      >
-        Next
-      </button>
-    </div>
-  )
+  return <div>Go to http://localhost:3000/wizard/</div>
 }
 
 export default Home
