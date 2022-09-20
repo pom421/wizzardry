a7b7c678dcc4c8d64569e2d4b791d230124f7278
pom421 <mauguet.po@gmail.com>
2022-09-20 17:29:54 +0200

    feat: remove unncessary getServerSideProps

--- src/machines/wizardMachine.ts
+++ src/machines/wizardMachine.ts
@@ -1,23 +1,40 @@
 import Router from "next/router"
 import React from "react"
+import { ContextFrom, EventFrom } from "xstate"
 import { createModel } from "xstate/lib/model"
+
 import { FormStep } from "../commons/types"
+
+type StepStatus = { label: string; status: "unreachable" | "visited" | "valid" | "invalid" | "obsolete" }
+
+export type WizardryContext = ContextFrom<typeof wizardModel>
+
+export type WizardryEvent = EventFrom<typeof wizardModel>
 
 const wizardModel = createModel(
   {
-    errorMessage: "toto" as string | undefined,
+    errorMessage: undefined as string | undefined,
     currentStep: undefined as FormStep<{ children: React.ReactNode }> | undefined,
     allSteps: [] as FormStep<unknown>[],
+    stepsStatus: [] as StepStatus[],
+    data: {} as Record<string, any>, // content of all forms in a form { form1: { name: "toto" }, form2: { age: 32 }}
   },
   {
     events: {
       goToPreviousPage: () => ({}),
       goToNextPage: () => ({}),
+      goToPage: ({ step }: { step: string }) => ({ step }),
     },
   },
 )
 
 const getIndex = (steps: FormStep<unknown>[], label: string) => steps.map((form) => form.label).indexOf(label)
+
+const statusForStep = (stepsStatus: StepStatus[], searchedStep: string) => {
+  const stepStatus = stepsStatus.filter((step) => step.label === searchedStep)
+
+  if (stepStatus.length) return stepStatus[0] || undefined
+}
 
 export const declarationMachine =
   /** @xstate-layout N4IgpgJg5mDOIC5QTAYwDYEMBOmAuAlgPYB2AspqgBYElgB0AwtmPrVAAR0Du9AylSLd2HAA6YYAYihEAKkQAKLAG7EArrAUSwiUKKKwChUrpAAPRAFoAjAE4AHPQDMAdgAMtt9ZdO3LgKwALABMLgA0IACeiABswW70oTFu-v7WbjFOgdYAvjkRKBg4bKQU1LQMzKyEJJw8-ILCtWLa0nJEAHJgZnhaMKb6hsYkphYITrYx9IFOwWkuMdb+wU5pMRHRCP62wfTWq7YTgf4utjv+efkgJEQo8EgghVi4w2U0dEwsbM31AkIi4n6D0GRmIIweY2swWs9Hsvhc1kC9kRyIywQ2iCRU3s2wmGQCq22gTyBTQzxK5Eo70qXxqdTA3AGBlBJghVmCHOc7k83nhQVCGIQ2UCsIR1n27nswXsbmJVyexVeVIqTKGYNG7MCU1cHi8Pj8-PCUUQWV29ilcJiLmy8SRLkuOSAA */
@@ -40,6 +57,10 @@ export const declarationMachine =
                   actions: ["Calculate next page", "Store in local storage", "Route to page"],
                   cond: "Is not the last page",
                 },
+                goToPage: {
+                  actions: ["Store in local storage", "Route to page"],
+                  cond: "Is the page reachable",
+                },
               },
             },
           },
@@ -53,6 +74,12 @@ export const declarationMachine =
         },
         "Is not the last page": (context) => {
           return context.currentStep.label !== context.allSteps[context.allSteps.length - 1].label
+        },
+        "Is the page reachable": (context, event) => {
+          if (event.type !== "goToPage") return
+
+          // return statusForStep(context.stepsStatus, event.step).status !== "unreachable"
+          return true
         },
       },
       actions: {
--- src/pages/wizard/[[...step]].page.tsx
+++ src/pages/wizard/[[...step]].page.tsx
@@ -1,29 +1,66 @@
 import { useMachine } from "@xstate/react"
 import type { NextPage } from "next"
+import { useRouter } from "next/router"
+import React from "react"
 import { Form1 } from "../../components/forms/Form1"
 import { Form2 } from "../../components/forms/Form2"
 import { FormEnd } from "../../components/forms/FormEnd"
 
-import { declarationMachine } from "../../machines/wizardMachine"
-
-export async function getServerSideProps({ query }) {
-  const { step } = query
-
-  return {
-    props: {
-      step,
-    },
-  }
-}
-
-// const EGAPRO_STEPS = ["page1", "page2", "page3", "page4"]
+import { declarationMachine, WizardryContext } from "../../machines/wizardMachine"
 
 const EGAPRO_STEPS = [Form1, Form2, FormEnd]
 
-const WizardPage: NextPage = ({ step }: { step: string[] }) => {
-  const index = EGAPRO_STEPS.map((form) => form.label).indexOf(step[0])
+type Props = {
+  children: React.ReactNode
+}
+
+// const ClientOnly: React.FC<Props> = ({ children }) => {
+//   const [isMounted, setMounted] = React.useState(false)
+
+//   React.useEffect(() => {
+//     setMounted(true)
+//   }, [])
+
+//   if (!isMounted) return null
+
+//   return <>{children}</>
+// }
+
+const WizardPage: NextPage = () => {
+  const router = useRouter()
+
+  const { step } = router.query
+
+  console.log("query", step)
+
+  const normalizedStep = Array.isArray(step) && step.length > 0 ? step[0] : undefined
+
+  return <Wizard step={normalizedStep} />
+}
+
+type WizardProps = {
+  step: string | undefined
+}
+
+// Show context with meaningful values.
+const DebugContext = ({ state }) => {
+  const context: WizardryContext = state.context
+
+  const res = {
+    ...context,
+    currentStep: context?.currentStep.label,
+    allSteps: context?.allSteps.map((step) => step.label),
+  }
+
+  return <pre>{JSON.stringify(res, null, 2)}</pre>
+}
+
+const Wizard: React.FC<WizardProps> = ({ step }) => {
+  const index = EGAPRO_STEPS.map((form) => form.label).indexOf(step)
 
   const normalizedStep = index >= 0 ? EGAPRO_STEPS[index] : EGAPRO_STEPS[0]
+
+  console.log("normalizedStep", normalizedStep.label)
 
   const [state, send] = useMachine(declarationMachine, {
     context: {
@@ -34,12 +71,24 @@ const WizardPage: NextPage = ({ step }: { step: string[] }) => {
 
   const { currentStep: CurrentForm } = state.context
 
+  React.useEffect(() => {
+    if (CurrentForm.label !== step) {
+      // todo: essayer de router dans la machine au step demandé
+      send({
+        type: "goToPage",
+        step,
+      })
+    }
+  }, [step, CurrentForm.label, send])
+
+  console.log("currentStep", CurrentForm.label)
+
   return (
     <>
       <h1>Dans wizard {step}</h1>
       <div>
         {JSON.stringify(state.value)} <br />
-        {JSON.stringify(state.context)}
+        <DebugContext state={state} />
         <br />
         <CurrentForm>
           <button
